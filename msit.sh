de111d47922dde98b35b8bf4607e1ad7389b884c
anilkumar9494 <99713438+anilkumar9494@users.noreply.github.com>
2022-04-11 10:17:12 +0530

    Update msit.sh

--- msit.sh
+++ msit.sh
@@ -5,52 +5,52 @@ echo "I will just append to my custom file" >>/var/log/
 cat msit.sh
 
 echo "old file dates logfile"
-sudo grep $(date +"%Y-%m-%d") alternatives.log
-sudo grep $(date +"%Y-%m-%d") dpkg.log
+grep $(date +"%Y-%m-%d") alternatives.log
+grep $(date +"%Y-%m-%d") dpkg.log
 
 echo "present date logfile"
-sudo grep $(date +"%Y-%m-%d") auth.log
-sudo grep $(date +"%Y-%m-%d") cloud-init-output.log
-sudo grep $(date +"%Y-%m-%d") kern.log
-sudo grep $(date +"%Y-%m-%d") syslog
-sudo grep $(date +"%Y-%m-%d") cloud-init.log
+grep $(date +"%Y-%m-%d") auth.log
+grep $(date +"%Y-%m-%d") cloud-init-output.log
+grep $(date +"%Y-%m-%d") kern.log
+grep $(date +"%Y-%m-%d") syslog
+grep $(date +"%Y-%m-%d") cloud-init.log
 
 echo "new logfile list"
 ls
 
 echo " create a backup file "
-sudo tar cf alternatives.log.tar alternatives.log
+tar cf alternatives.log.tar alternatives.log
 echo "create a backupfile "
-sudo tar cf dpkg.log.tar dpkg.log
+tar cf dpkg.log.tar dpkg.log
 
 echo "your directory code is created successfully"
-sudo mkdir backup
+mkdir backup
 
 echo "file move to folder"
-sudo mv dpkg.log.tar backup
+mv dpkg.log.tar backup
 echo "file move to folder"
-sudo mv alternatives.log.tar backup
+mv alternatives.log.tar backup
 
 echo "file move to folder"
-sudo mv kern.log. backup
+mv kern.log. backup
 echo "file move to folder"
-sudo mv auth.log backup
+mv auth.log backup
 echo "file move to folder"
-sudo mv cloud-init-output.log backup
+mv cloud-init-output.log backup
 echo "file move to folder"
-sudo mv cloud-init.log backup
+mv cloud-init.log backup
 
 cd  /var/log/backup
 
 echo "create a directory"
-sudo mkdir dated
+mkdir dated
 
 cd /var/log/backup/
 
 echo "file move to folder"
-sudo mv dpkg.log.tar dated
+mv dpkg.log.tar dated
 echo "file move to folder"
-sudo mv alternatives.log.tar dated
+mv alternatives.log.tar dated
 
 echo "go to path "
 cd /var/log/backup/dated
